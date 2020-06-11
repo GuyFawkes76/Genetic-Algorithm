11d17a0fde949372c954bb349da3a406b3143845
Heirat <neocron76@gmail.com>
2020-06-11 18:31:22 +0300

    Создал пустой файл си

---
+++ Genetic_algoritm/Source.c
@@ -0,0 +1,16 @@
+/*
+Лабораторная работа 5. Генетический Алгоритм.
+Червинский Артём, Степаненко Кирилл, ИВТ-13БО
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+
+	system("pause");
+	return 0;
+}
