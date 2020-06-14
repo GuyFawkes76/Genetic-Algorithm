fbf31a9677d946d7402144bd8a15b0f873da8a48
Heirat <neocron76@gmail.com>
2020-06-14 15:13:19 +0300

    Добавил структуры данных

--- Genetic-Algorithm/Source.c
+++ Genetic-Algorithm/Source.c
@@ -6,9 +6,73 @@
 #include <stdio.h>
 #include <locale.h>
 
+#define GENOM_LEN 64
+#define F_SIZE_L 45
+#define F_SIZE_W 20
+
+// Все данные бота
+struct bot
+{
+	int genom[GENOM_LEN];
+	int id; // Идентификатор 0..63
+	int alive; // жив ли бот
+	int r, c; // row и col, координаты бота
+	int hp; // Число очков жизни 0..99
+	int view; // Направление взгляда 0..7
+	struct bot *next;
+};
+
+// Список ботов
+typedef struct bots
+{
+	struct bot *first;	
+} Bots;
+
 
 // Загружает или создает новую конфигурацию симуляции
 int Init()
+{
+
+}
+
+
+// Нужно ли создавать новое поколение
+int isEndGeneration ()
+{
+
+}
+
+
+// Обрабатывает ходы ботов
+int HandleBots ()
+{
+
+}
+
+
+// Выводит поле и доп информацию
+int PrintInfo ()
+{
+
+}
+
+
+// Проверяет, нажал ли пользователь на паузу
+int isPause ()
+{
+
+}
+
+
+// Останавливает симуляцию
+int StopSim ()
+{
+
+}
+
+
+// Уменьшает скорость мелькания экрана
+int LittlePause ()
 {
 
 }
@@ -24,14 +88,14 @@ int MainCycle ()
 		while (!isEndGeneration ())  // Пока на поле больше 8 ботов
 		{
 			HandleBots ();  // Обработать ходы ботов
-			PrintField ();  // Вывести измененное поле
+			PrintInfo ();  // Вывести измененное поле
 			if (isPause ())  // Если пользователь остановил симуляцию
 			{
 				StopSim ();  // Пауза, выход или сохранение в файл
 			}
 			else
 			{
-				LittlePause ()  // Небольшой Sleep (50), чтобы не мелькал экран
+				LittlePause ();  // Небольшой Sleep (50), чтобы не мелькал экран
 			}
 		}
 	}
@@ -49,7 +113,7 @@ int main() {
 	setlocale(LC_ALL, "RUS");
 	
 	Init ();
-	MainСycle ();
+	MainCycle ();
 	Result ();
 
 	system("pause");
