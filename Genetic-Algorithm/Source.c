1b0c2d00dd0aab9d297eebcd6f64242eebbb2c0a
Кирилл Степаненко <kirill-stepanenko22@hotmail.com>
2020-07-01 22:03:44 +0300

    Убрал автоматическую итерацию ботов в цикле.

--- Genetic-Algorithm/Source.c
+++ Genetic-Algorithm/Source.c
@@ -50,22 +50,22 @@ int isAlive(Bot * bot) {
 // Обрабатывает ходы ботов
 int handleBots (char Field[F_SIZE_VERT][F_SIZE_HOR], Bots * botlist) {
 	Bot* curBot;
-	for (curBot = botlist->first; curBot; curBot = curBot->next) {
+	for (curBot = botlist->first; curBot; ) {
 		if (isAlive(curBot)) {
 			if (curBot->genom[curBot->curCmd] <= 7) {
-				move();
+				move(Field, curBot);
 			}
 			if ((curBot->genom[curBot->curCmd] >= 8) && (curBot->genom[curBot->curCmd] <= 15))  {
-				grab();
+				grab(Field, curBot);
 			}
 			if ((curBot->genom[curBot->curCmd] >= 16) && (curBot->genom[curBot->curCmd] <= 23)) {
-				seek();
+				seek(Field, curBot);
 			}
 			if ((curBot->genom[curBot->curCmd] >= 24) && (curBot->genom[curBot->curCmd] <= 31)) {
-				turn();
+				turn(Field, curBot);
 			}
 			if ((curBot->genom[curBot->curCmd] >= 32) && (curBot->genom[curBot->curCmd] <= 63)) {
-				genomeStep();
+				genomeStep(Field, curBot);
 			}
 		}
 	}
